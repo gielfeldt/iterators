3276291d4b99c7e5a6c5af5ccdf0e0180f4db3da
Thomas Gielfeldt <thomas@gielfeldt.dk>
2017-04-09 14:41:48 +0200

    Make it possible to map keys as well as values.

--- src/MapIterator.php
+++ src/MapIterator.php
@@ -4,15 +4,45 @@ namespace Gielfeldt\Iterators;
 
 class MapIterator extends \IteratorIterator
 {
+    protected $currentKey;
+    protected $currentValue;
+
     public function __construct(\Traversable $iterator, callable $callback)
     {
         parent::__construct($iterator);
         $this->callback = \Closure::fromCallable($callback);
     }
 
+    private function map()
+    {
+        if ($this->valid()) {
+            list($this->currentKey, $this->currentValue) = ($this->callback)($this->getInnerIterator());
+        }
+        else {
+            $this->currentKey = null;
+            $this->currentValue = null;
+        }
+    }
+
+    public function rewind()
+    {
+        parent::rewind();
+        $this->map();
+    }
+
+    public function next()
+    {
+        parent::next();
+        $this->map();
+    }
+
+    public function key()
+    {
+        return $this->currentKey;
+    }
+
     public function current()
     {
-        $current = parent::current();
-        return $current ? ($this->callback)($current) : $current;
+        return $this->currentValue;
     }
 }
