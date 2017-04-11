35b52e36247723e3dc2f60f18c31bec3fa366966
Thomas Gielfeldt <thomas@gielfeldt.dk>
2017-04-11 21:52:28 +0200

    Test countable iterator in countable iterator.

--- tests/CountableIteratorTest.php
+++ tests/CountableIteratorTest.php
@@ -10,10 +10,14 @@ class CountableIteratorTest extends IteratorsTestBase
     {
         $input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20];
 
-        $iterator = new \IteratorIterator(new \ArrayIterator($input));
+        $arrayIterator = new \ArrayIterator($input);
+        $iterator = new \IteratorIterator($arrayIterator);
         $result = new CountableIterator($iterator);
         $this->assertEquals(10, count($input), 'Input array is of wrong size.');
         $this->assertEquals(1, count($iterator), 'IteratorIterator should not be countable.');
+        $this->assertEquals(10, count($result), 'CountableIterator did not count correctly.');
+
+        $result = new CountableIterator($arrayIterator);
         $this->assertEquals(10, count($result), 'CountableIterator did not count correctly.');
     }
 }
