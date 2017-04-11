08ab9f6400962f259f89e70c2a59fc508a2cf559
Thomas Gielfeldt <thomas@gielfeldt.dk>
2017-04-11 06:33:11 +0200

    Add some more helper methods.

--- src/IteratorHelper.php
+++ src/IteratorHelper.php
@@ -14,4 +14,18 @@ class IteratorHelper
         }
         return false;
     }
+
+    public static function getInnerIterators(\Traversable $iterator)
+    {
+        if ($iterator instanceof \OuterIterator) {
+            return array_merge([$iterator], self::getInnerIterators($iterator->getInnerIterator()));
+        }
+        return [];
+    }
+
+    public static function getInnermostIterator(\Traversable $iterator)
+    {
+        $iterators = self::getInnerIterators($iterator);
+        return $iterators ? end($iterators) : null;
+    }
 }
