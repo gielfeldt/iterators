4c3cf7660d472e5e1bedbb5f9915b3c2d1171671
Thomas Gielfeldt <thomas@gielfeldt.dk>
2017-04-11 07:36:50 +0200

    Add more helper methods.

--- src/IteratorHelper.php
+++ src/IteratorHelper.php
@@ -28,4 +28,44 @@ class IteratorHelper
         $iterators = self::getInnerIterators($iterator);
         return $iterators ? end($iterators) : null;
     }
+
+    public static function reduce(\Traversable $iterator, callable $callback, $initial = null)
+    {
+        $callback = \Closure::fromCallable($callback);
+        iterator_apply($iterator, function ($iterator) use (&$initial, $callback) {
+            $initial = $callback($initial, $iterator->current(), $iterator->key());
+            return true;
+        }, [$iterator]);
+        return $initial;
+    }
+
+    public static function sum(\Traversable $iterator)
+    {
+        return self::reduce($iterator, function ($carry, $value, $key) {
+            return $carry + $value;
+        }, 0);
+    }
+
+    public static function product(\Traversable $iterator)
+    {
+        return self::reduce($iterator, function ($carry, $value, $key) {
+            return $carry * $value;
+        }, 1);
+    }
+
+    public static function concatenate(\Traversable $iterator)
+    {
+        return self::reduce($iterator, function ($carry, $value, $key) {
+            return $carry . $value;
+        }, '');
+    }
+
+    public static function implode($separator, \Traversable $iterator)
+    {
+        $result = self::reduce($iterator, function ($carry, $value, $key) use ($separator) {
+            return $carry . $value . $separator;
+        }, '');
+        $result = mb_substr($result, 0, -mb_strlen($separator));
+        return $result;
+    }
 }
