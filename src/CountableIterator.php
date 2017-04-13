c1f71332c82e747027fb508458501350509f1f84
Thomas Gielfeldt <thomas@gielfeldt.dk>
2017-04-13 13:03:50 +0200

    Add caching to count.

--- src/CountableIterator.php
+++ src/CountableIterator.php
@@ -4,22 +4,32 @@ namespace Gielfeldt\Iterators;
 
 class CountableIterator extends \IteratorIterator implements \Countable
 {
-    public function __construct(\Traversable $iterator)
+    const CACHE_COUNT = 1;
+
+    protected $count;
+    protected $flags;
+
+    public function __construct(\Traversable $iterator, $flags = self::CACHE_COUNT)
     {
+        $this->flags = $flags;
         parent::__construct($iterator);
     }
 
     public function count()
     {
-        if ($this->getInnerIterator() instanceof \Countable) {
-            return $this->getInnerIterator()->count();
+        if (!($this->flags & self::CACHE_COUNT) || !isset($this->count)) {
+            if ($this->getInnerIterator() instanceof \Countable) {
+                $this->count = intval($this->getInnerIterator()->count());
+            }
+            else {
+                $this->count = 0;
+                $this->rewind();
+                while ($this->valid()) {
+                    $this->count++;
+                    $this->next();
+                }
+            }
         }
-        $count = 0;
-        $this->rewind();
-        while ($this->valid()) {
-            $count++;
-            $this->next();
-        }
-        return $count;
+        return $this->count;
     }
 }
--- tests/CountableIteratorTest.php
+++ tests/CountableIteratorTest.php
@@ -6,7 +6,7 @@ use Gielfeldt\Iterators\CountableIterator;
 
 class CountableIteratorTest extends IteratorsTestBase
 {
-    public function testCountableIterator()
+    public function testCountableIteratorCached()
     {
         $input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20];
 
@@ -17,7 +17,32 @@ class CountableIteratorTest extends IteratorsTestBase
         $this->assertEquals(1, count($iterator), 'IteratorIterator should not be countable.');
         $this->assertEquals(10, count($result), 'CountableIterator did not count correctly.');
 
-        $result = new CountableIterator($arrayIterator);
+        $result2 = new CountableIterator($arrayIterator);
+        $this->assertEquals(10, count($result2), 'CountableIterator did not count correctly.');
+
+        $arrayIterator->append('test');
+        $this->assertEquals(11, count($arrayIterator), 'CountableIterator did not count correctly.');
+        $this->assertEquals(10, count($result2), 'CountableIterator did not count correctly.');
         $this->assertEquals(10, count($result), 'CountableIterator did not count correctly.');
+    }
+
+    public function testCountableIteratorUncached()
+    {
+        $input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 20];
+
+        $arrayIterator = new \ArrayIterator($input);
+        $iterator = new \IteratorIterator($arrayIterator);
+        $result = new CountableIterator($iterator, 0);
+        $this->assertEquals(10, count($input), 'Input array is of wrong size.');
+        $this->assertEquals(1, count($iterator), 'IteratorIterator should not be countable.');
+        $this->assertEquals(10, count($result), 'CountableIterator did not count correctly.');
+
+        $result2 = new CountableIterator($arrayIterator, 0);
+        $this->assertEquals(10, count($result2), 'CountableIterator did not count correctly.');
+
+        $arrayIterator->append('test');
+        $this->assertEquals(11, count($arrayIterator), 'CountableIterator did not count correctly.');
+        $this->assertEquals(11, count($result2), 'CountableIterator did not count correctly.');
+        $this->assertEquals(11, count($result), 'CountableIterator did not count correctly.');
     }
 }
