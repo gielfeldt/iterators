c395970451acac6d5a1e967469abe8d37084277b
Thomas Gielfeldt <thomas@gielfeldt.dk>
2017-04-19 14:46:28 +0200

    Add tests.

---
+++ tests/InfiniteIteratorTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Gielfeldt\Tests\Iterators;
+
+use Gielfeldt\Iterators\InfiniteIterator;
+use Gielfeldt\Iterators\KeysIterator;
+use Gielfeldt\Iterators\ValuesIterator;
+
+class InfiniteIteratorTest extends IteratorsTestBase
+{
+    public function testInfiniteIterator()
+    {
+        $input = [1, 2, 3, 4];
+        $iterator = new InfiniteIterator(new \ArrayIterator($input));
+
+        $iterator = new \LimitIterator($iterator, 0, 10);
+        $expectedKeys = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1];
+        $expectedValues = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2];
+
+        $this->assertEquals($expectedKeys, iterator_to_array(new KeysIterator($iterator)), 'Limited infinite iterator did not work, keys are wrong.');
+        $this->assertEquals($expectedValues, iterator_to_array(new ValuesIterator($iterator)), 'Limited infinite iterator did not work, values are wrong.');
+    }
+
+    public function testInfiniteIteratorReindex()
+    {
+        $input = [1, 2, 3, 4];
+        $iterator = new InfiniteIterator(new \ArrayIterator($input), InfiniteIterator::REINDEX);
+
+        $iterator = new \LimitIterator($iterator, 0, 10);
+        $expectedKeys = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+        $expectedValues = [1, 2, 3, 4, 1, 2, 3, 4, 1, 2];
+
+        $this->assertEquals($expectedKeys, iterator_to_array(new KeysIterator($iterator)), 'Limited infinite iterator did not work, keys are wrong.');
+        $this->assertEquals($expectedValues, iterator_to_array(new ValuesIterator($iterator)), 'Limited infinite iterator did not work, values are wrong.');
+    }
+}
