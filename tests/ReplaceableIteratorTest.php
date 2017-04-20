379f64181c0a35dadd350a0e305db6d8dbf0985e
Thomas Gielfeldt <thomas@gielfeldt.dk>
2017-04-20 18:09:58 +0200

    Improve test coverage.

--- tests/ReplaceableIteratorTest.php
+++ tests/ReplaceableIteratorTest.php
@@ -12,8 +12,10 @@ class ReplaceableIteratorTest extends IteratorsTestBase
 
         $iterator->setInnerIterator(new \ArrayIterator(range(1, 10)));
         $this->assertEquals(range(1, 10), iterator_to_array($iterator), 'Inner iterator was not set correctly.');
+        $this->assertEquals(range(1, 10), $iterator->getArrayCopy(), 'Call to inner iterator not dispatched.');
 
         $iterator->setInnerIterator(new \ArrayIterator(range(11, 20)));
         $this->assertEquals(range(11, 20), iterator_to_array($iterator), 'Inner iterator was not set correctly.');
+        $this->assertEquals(range(11, 20), $iterator->getArrayCopy(), 'Call to inner iterator not dispatched.');
     }
 }
